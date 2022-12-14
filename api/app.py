3a868bf69fa0ea06733a5ea076cf934f38728c6a
wesleyfralima <wesleyfralima@gmail.com>
2022-12-14 19:12:07 -0300

    Atualizado o projeto.

--- api/app.py
+++ api/app.py
@@ -46,11 +46,6 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Configure session to use filesystem (instead of signed cookies)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # Configure CS50 Library to use SQLite database
