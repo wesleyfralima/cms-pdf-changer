9b82fc4d97923c7537248062aa4084fef3191597
wesleyfralima <wesleyfralima@gmail.com>
2022-12-14 19:14:17 -0300

    Atualizado o projeto.

--- api/app.py
+++ api/app.py
@@ -79,9 +79,6 @@ def index():
 def register():
     """Register new user."""
 
-    # Forget any user_id
-    session.clear()
-
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
         username = request.form.get("username")
@@ -131,9 +128,6 @@ def register():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
-    # Forget any user_id
-    session.clear()
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
